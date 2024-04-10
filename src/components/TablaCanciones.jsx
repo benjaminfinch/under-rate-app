0a9e251c008cf00b30a0ec3af625506e59a4cbab
Diego Fuenzalida <fuenzaladeveloper@gmail.com>
2024-04-10 13:53:45 -0400

    Zustland iniciado

--- src/components/TablaCanciones.jsx
+++ src/components/TablaCanciones.jsx
@@ -1,20 +1,31 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { useMusicStore } from "../store/musicStore";
+
+
 
 export const TablaCanciones = () => {
   const [songs, setSongs] = useState([]);
   const params = useParams();
+  const setMusic = useMusicStore((state) => state.setMusic)
+  // const removeMusic = useMusicStore((state) => state.removeAllMusic)
+  const useMusicStore((state) => state.music)
 
-  
   useEffect(() => {
-    fetch(`https://web-production-3722.up.railway.app/songs/${params.user_id}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
+    fetch(
+      `https://web-production-3722.up.railway.app/songs/${params.user_id}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
       .then((response) => response.json())
       .then((data) => {
+        // removeMusic()
+        setMusic(data);
+
         setSongs(data);
       });
   }, []);
@@ -23,7 +34,7 @@ export const TablaCanciones = () => {
     <>
       <br />
       <div className="container">
-        <table class="table">
+        <table className="table">
           <thead>
             <tr>
               <th scope="col">#</th>
@@ -34,20 +45,19 @@ export const TablaCanciones = () => {
           </thead>
           <tbody>
             {songs.map((song) => {
-                return (
-                    <tr>
-                    <th scope="row">{song.song_id}</th>
-                    <td>{song.song_name}</td>
-                    <td>{song.average_ratings}</td>
-                    <td>
-                        <audio controls>
-                            <source src={song.sound_archive} type="audio/mpeg" />
-                        </audio>
-                    </td>
-                    </tr>
-                );
+              return (
+                <tr>
+                  <th scope="row">{song.song_id}</th>
+                  <td>{song.song_name}</td>
+                  <td>{song.average_ratings}</td>
+                  <td>
+                    <audio controls>
+                      <source src={song.sound_archive} type="audio/mpeg" />
+                    </audio>
+                  </td>
+                </tr>
+              );
             })}
-           
           </tbody>
         </table>
       </div>
