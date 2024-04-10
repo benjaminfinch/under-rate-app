1c4825b3dc1bce22c7af083f4cb5a2976ee5a57e
Benjamin Finch <benjaminfinch@skiff.com>
2024-04-10 17:44:14 -0400

    Update Bloque_1_nosotros.jsx

--- src/components/Bloque_1_nosotros.jsx
+++ src/components/Bloque_1_nosotros.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import Image from 'react-bootstrap/Image';
 import ImagenBloqueUno from '../assets/images/imagenBloque1.png'
 
+
+const tab = '\u00A0';
+
 const Bloque_1_Nosotros = () => {
   return (
     <>
     <div className='container pt-60 pb-60'>
        <div className='row'>          
           <div className='col-lg-6 col-md-12 col-sm-6 '>
-               <h1>Acerca de Under Rate</h1>
-            <li></li>
-            <li></li>
             <h2>Misión</h2>
             <p>Desarrollar y fortalecer una relación más cercana entre los artistas emergentes con su audiencia y ayudarlos a alcanzar su máximo potencial creativo.</p>
             <h2>Visión</h2>
