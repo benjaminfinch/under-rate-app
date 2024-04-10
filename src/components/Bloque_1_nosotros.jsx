c97b67d75549793b33d59df920576f8a1b0231cd
Benjamin Finch <benjaminfinch@skiff.com>
2024-04-10 17:22:59 -0400

    Update Bloque_1_nosotros.jsx

--- src/components/Bloque_1_nosotros.jsx
+++ src/components/Bloque_1_nosotros.jsx
@@ -9,9 +9,9 @@ const Bloque_1_Nosotros = () => {
        <div className='row'>          
           <div className='col-lg-6 col-md-12 col-sm-6 '>
                <h1>Acerca de Under Rate</h1>
+            <li></li>
+            <li></li>
             <h2>Misión</h2>
-            <div></div>
-            <div></div>
             <p>Desarrollar y fortalecer una relación más cercana entre los artistas emergentes con su audiencia y ayudarlos a alcanzar su máximo potencial creativo.</p>
             <h2>Visión</h2>
             <p>Queremos ser el punto de partida para los músicos que buscan construir su audiencia y hacerse un nombre en la escena musical.</p>
